ee1f30b4afbb4142f44026d076f1fa2cabb31365
pm-erik <erik.bachorski@pmagentur.com>
2026-05-07 10:34:03 +0200

    editor: gate /_preview/pages/{id} to dev environment

--- src/Controller/PageBuilderPreviewController.php
+++ src/Controller/PageBuilderPreviewController.php
@@ -10,16 +10,28 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Dev-only standalone preview of the editor bundle in page-data mode.
+ * The production editor render path is ibexa's page-builder iframe (twig
+ * overrides + marker-mode bundle); this route exists for local iteration
+ * without logging into admin and is 404'd in non-dev environments.
+ */
 #[Route('/_preview', name: 'app_preview_')]
 final class PageBuilderPreviewController extends AbstractController
 {
-    public function __construct(private readonly PageRepositoryInterface $pages)
-    {
+    public function __construct(
+        private readonly PageRepositoryInterface $pages,
+        private readonly string $kernelEnvironment,
+    ) {
     }
 
     #[Route('/pages/{locationId}', name: 'page', methods: ['GET'], requirements: ['locationId' => '\d+'])]
     public function preview(int $locationId): Response
     {
+        if ($this->kernelEnvironment !== 'dev') {
+            throw new NotFoundHttpException();
+        }
+
         $page = $this->pages->findByLocationId($locationId);
         if ($page === null) {
             throw new NotFoundHttpException(sprintf('No page builder content at location %d', $locationId));
